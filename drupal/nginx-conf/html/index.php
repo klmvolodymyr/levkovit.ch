e353e401bf72ec36749316b26eb2b5bc4904cbb1
Volodymyr Klymniuk <Volodymyr.Klymniuk@gmail.com>
2023-09-25 13:53:38 +0300

    start

---
+++ drupal/nginx-conf/html/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
+//var_dump('21');
+@error_reporting(E_ALL);
+
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+
+//die;
+$autoloader = require_once 'autoload.php';
+
+$kernel = new DrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
